11861c3981207d3a8d1ba1784e06d338b7135dc6
ibraheem-111 <125667543+ibraheem-111@users.noreply.github.com>
2023-04-28 18:13:00 +0500

    modified:   .env 	new file:   src/utils/auth.middleware.ts

---
+++ src/utils/auth.middleware.ts
@@ -0,0 +1,35 @@
+import { HttpException, HttpStatus, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Response, Request } from "express";
+
+export class AuthMiddleware implements NestMiddleware {
+    constructor() {   
+    }
+
+    async getToken(req:Request) {
+        const {headers} = req;
+        const {authorization=null}= headers;
+        const split_header : string[] = authorization?.split(' ');
+        const [, token = null] = split_header ? split_header : [];
+        return {
+            authToken: token,
+        };
+    }
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        try{
+            const authToken = this.getToken(req)
+            const {UNAUTHORIZED}    =   HttpStatus
+            if(!authToken){
+                throw new HttpException(
+                    'Unauthorized',
+                    UNAUTHORIZED
+                )
+            }
+
+            next();
+        }
+        catch(err){
+            console.log('middleware auth', err);
+        }
+    }
+}
